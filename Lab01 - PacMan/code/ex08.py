439d69524d775481ab604bcbde0b2610c65634fe
Thomas Menchi <thomas.menchi@edu.esiee.fr>
2021-12-01 13:41:30 +0100

    ex08

--- Lab01 - PacMan/code/ex08.py
+++ Lab01 - PacMan/code/ex08.py
@@ -19,6 +19,24 @@ def value_iteration(mdp, discount, iterations):
 
     for k in range(iterations):
         "*** YOUR CODE HERE ***"
-        raiseNotDefined()
+
+        for _ in range(iterations):
+            for state in mdp.getStates():
+                if mdp.isTerminal(state): v_table[state] = 0
+                else :
+                    max = -1
+                    for action in mdp.getPossibleActions(state):
+                        if q_table[state][action] > max : max = q_table[state][action]
+                    v_table[state] = max
+
+            for state in mdp.getStates():
+                for action in mdp.getPossibleActions(state):
+                    q = 0
+                    for next_state, prob in mdp.getTransitionStatesAndProbs(state,action):
+                        R = mdp.getReward(state, action, next_state)
+                        q += prob * (R + discount * v_table[next_state])
+                    q_table[state][action] = q
+
+
 
     return q_table
