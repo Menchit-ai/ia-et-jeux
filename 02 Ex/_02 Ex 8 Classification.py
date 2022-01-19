793c98c7c2966da522d09482e2ec3ecb6bc6f7ce
Thomas Menchi <thomas.menchi@edu.esiee.fr>
2022-01-19 13:36:07 +0100

    ex08

--- 02 Ex/_02 Ex 8 Classification.py
+++ 02 Ex/_02 Ex 8 Classification.py
@@ -6,47 +6,78 @@ from   torchvision import datasets, transforms
 """
 ####  1 couche Linear
 #	Qu1 : quel est le % de bonnes prédictions obtenu au lancement du programme , pourquoi ?
+Au lancement du programme on obtient entre 7.5% et entre 15%. Ces faibles valeurs sont dûes
+aux poids aléatoire dans les neurones (le réseau n'est pas encore entraîné).
+
 #	Qu2 : quel est le % de bonnes prédictions obtenu avec 1 couche Linear ?
+On obtient rapidement (au bout de deux-trois epochs) 90%
+
 #	Qu3 : pourquoi le test_loader n’est pas découpé en batch ?
+On veut tester sur toute notre base de test et non pas seulement sur une partie (un batch)
+afin d'avoir la mesure la plus précise.
+
 #   Qu4 : pourquoi la couche Linear comporte-t-elle 784 entrées ?
+C'est le nombre de features à calculer le plus bas possible afin d'avoir un réseau performant.
+
 #   Qu5 : pourquoi la couche Linear comporte-t-elle 10 sorties ?
+Les images appartiennent à 10 classes distinctes.
 
 ####  2 couches Linear
 #   Qu6 : quelles sont les tailles des deux couches Linear ?
+La première a pour taille (784, 128) et la seconde a pour taille (128, 10).
+
 # 	Qu7 : quel est l’ordre de grandeur du nombre de poids utilisés dans ce réseau ?
+On a un ordre de grandeur de 10^6
+
 #	Qu8 : quel est le % de bonnes prédictions obtenu avec 2 couches Linear ?
+97%
 
 ####  3 couches Linear
 #   Qu9 : obtient-on un réel gain sur la qualité des prédictions ?
+On obtient 97% de précision comme dans le cas précédent
 
 ####  Fonction Softmax
 #   Qu10 : pourquoi est il inutile de changer le code de la fonction TestOK ?
-
+Dans TestOK on ne fait que compter le nombre de bons résultats, on ne tient pas compte
+des probabilités attribuées par le réseau.
 """
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.FC1 = nn.Linear(784, 10)
+        self.FC1 = nn.Linear(784, 128)
+        self.FC2 = nn.Linear(128, 64)
+        self.FC3 = nn.Linear(64, 10)
+        self.relu = nn.functional.relu
+        self.softmax = F.log_softmax
 
     def forward(self, x):
         n = x.shape[0]
         x = x.reshape((n,784))
-        output = self.FC1(x)
+
+        v1 = self.FC1(x)
+        activ1 = self.relu(v1)
+
+        v2 = self.FC2(activ1)
+        activ2 = self.relu(v2)
+
+        v3 = self.FC3(activ2)
+        output = self.softmax(v3, dim=1)
         return output
 
 
     def Loss(self,Scores,target):
-        nb = Scores.shape[0]
-        TRange = torch.arange(0,nb,dtype=torch.int64)
-        scores_cat_ideale = Scores[TRange,target]
-        scores_cat_ideale = scores_cat_ideale.reshape(nb,1)
-        delta = 1
-        Scores = Scores + delta - scores_cat_ideale
-        x = F.relu(Scores)
-        err = torch.sum(x)
-        return err
+        # nb = Scores.shape[0]
+        # TRange = torch.arange(0,nb,dtype=torch.int64)
+        # scores_cat_ideale = Scores[TRange,target]
+        # scores_cat_ideale = scores_cat_ideale.reshape(nb,1)
+        # delta = 1
+        # Scores = Scores + delta - scores_cat_ideale
+        # x = F.relu(Scores)
+        # err = torch.sum(x)
+        # return err
+        return F.nll_loss(Scores, target)
 
 
     def TestOK(self,Scores,target):
