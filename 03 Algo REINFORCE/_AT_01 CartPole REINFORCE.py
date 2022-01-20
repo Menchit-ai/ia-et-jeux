63216287960dcea7a961ba879b7bf8a5e09db8fe
Thomas Menchi <thomas.menchi@edu.esiee.fr>
2022-01-20 23:01:02 +0100

    reinforce

--- 03 Algo REINFORCE/_AT_01 CartPole REINFORCE.py
+++ 03 Algo REINFORCE/_AT_01 CartPole REINFORCE.py
@@ -1,5 +1,10 @@
-import gym
 import time
+from email import policy
+
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from pyglet.window import key
 
 env = gym.make('CartPole-v0')
@@ -9,29 +14,48 @@ env = gym.make('CartPole-v0')
 # si le charriot est à gauche du point de départ, alors => à droite
 # si le charriot est à droite du point de départ, alors => à gauche
 
-
 def GetAction(x):
     if x < 0 : return 1
     else : return 0
 
+layer1 = nn.Linear(4,20)
+layer2 = nn.Linear(20,2)
+softmax = nn.Softmax()
+
+def get_action_v2(observation):
+    x = torch.Tensor(observation)
+    x = layer1(x)
+    x = layer2(x)
+    x = softmax(x)
+    m = torch.distributions.categorical.Categorical(x)
+    index_action = m.sample()
+    log_prob = m.log_prob(index_action)
+    return index_action, log_prob
 
 # Main game loop
-
-while True :
+# optimizer = torch.optim.Adam(policy.parameters(), lr=0.001)
+for i in range(40) :
     done = False
     observation = env.reset()
     TotalScore = 0
     env.reset()
+    reward = []
+    log_prob = []
 
     while not done  :
-        w = env.render()
+        if i%20 == 0 : w = env.render()
         x = observation[0]
 
-        action = GetAction(x)
+        action, log_prob = get_action_v2(observation)
         observation, reward, done, info = env.step(action)
         TotalScore += reward
-        time.sleep(0.02)
 
+        reward.append(TotalScore)
+        log_prob.append(log_prob)
+
+        if i%20 == 0 : time.sleep(0.02)
+
+    optimizer.zero_grad()
 
     print("Score final : " , TotalScore)
 
